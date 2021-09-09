02fb065e57cf3612d002636c2881b7dd470ef1ef
Plagueonic <Plagueonic@gmail.com>
2021-09-08 21:42:57 -0600

    made the chart editor sound a billion times better
    swaggin

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -1176,11 +1176,10 @@ class ChartingState extends MusicBeatState
 				if(note.strumTime > lastConductorPos && ((playSoundBf.checked && note.mustPress) || (playSoundDad.checked && !note.mustPress)) && FlxG.sound.music.playing && note.noteData > -1) {
 					var data:Int = note.noteData % 4;
 					if(!playedSound[data]) {
-						var soundToPlay = 'Charting_';
+						var soundToPlay = 'Charting_1';
 						if(_song.player1 == 'gf') { //Easter egg
 							soundToPlay = 'GF_';
 						}
-						soundToPlay += Std.string(data + 1);
 						FlxG.sound.play(Paths.sound(soundToPlay));
 						playedSound[data] = true;
 					}
