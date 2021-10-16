2017be7969d671ab38790027b6e619de54ee6e6e
ThatOneCalculator <kainoa@t1c.dev>
2021-10-16 14:29:43 -0700

    mod ver in title screen

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -23,6 +23,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
+	public static var modVersion:String = '1.0.1';
 	public static var psychEngineVersion:String = '0.3.2'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
@@ -105,6 +106,10 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Friday Night Faxxin' v" + modVersion, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
