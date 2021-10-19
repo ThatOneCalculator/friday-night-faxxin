4b570687375a9a001fd27f7b197f41a03fe94de4
ThatOneCalculator <kainoa@t1c.dev>
2021-10-19 13:16:05 -0700

    Discord RPC

--- source/Discord.hx
+++ source/Discord.hx
@@ -75,7 +75,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: largeImageKey,
-			largeImageText: "Psych Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Friday Night Faxxin' v " + MainMenuState.modVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
